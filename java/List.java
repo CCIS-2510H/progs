60376f7aae30bfe404367b2aacd9864a30804ca4
David Van Horn <dvanhorn@ccs.neu.edu>
2013-02-28 14:01:31 -0500

    Untabify.

--- java/List.java
+++ java/List.java
@@ -8,14 +8,14 @@ interface Fun<X,Y> {
 
 class Add1 implements Fun<Integer,Integer> {
     public Integer apply(Integer x) {
-	return x+1;
+        return x+1;
     }
 }
 
 class IntToStr implements Fun<Integer,String> {
     IntToStr() {}
     public String apply(Integer x) {
-	return x.toString();
+        return x.toString();
     }
 }
 
@@ -34,7 +34,7 @@ interface ListVisitor<X,Y> {
 class GetFirst implements ListVisitor<Integer,Integer> {
     public Integer visitEmpty() { throw new RuntimeException("You suck"); }
     public Integer visitCons(Integer first, List<Integer> rest) {
-	return first;
+        return first;
     }
 }
 
@@ -43,19 +43,19 @@ class Empty<X> implements List<X> {
     Empty() {}
 
     public List<X> rev() {
-	return new Empty<X>();
+        return new Empty<X>();
     }
 
     public List<X> revAcc(List<X> acc) {
-	return acc;
+        return acc;
     }
 
     public <Y> List<Y> map(Fun<X,Y> f) {
-	return new Empty<Y>();
+        return new Empty<Y>();
     }
 
     public <Y> Y accept(ListVisitor<X,Y> v) {
-	return v.visitEmpty();
+        return v.visitEmpty();
     }
 }
 
@@ -65,30 +65,30 @@ class Cons<X> implements List<X> {
     List<X> rest;
 
     Cons(X first, List<X> rest) {
-	this.first = first;
-	this.rest = rest;
+        this.first = first;
+        this.rest = rest;
     }
 
     Cons(X first) {
-	this.first = first;
-	this.rest = new Empty<X>();
+        this.first = first;
+        this.rest = new Empty<X>();
     }
 
     public List<X> rev() {
-	return this.revAcc(new Empty<X>());
+        return this.revAcc(new Empty<X>());
     }
 
     public List<X> revAcc(List<X> acc) {
-	return this.rest.revAcc(new Cons<X>(this.first, acc));
+        return this.rest.revAcc(new Cons<X>(this.first, acc));
     }
 
     public <Y> List<Y> map(Fun<X,Y> f) {
-	return new Cons<Y>(f.apply(this.first),
-			   this.rest.map(f));
+        return new Cons<Y>(f.apply(this.first),
+                           this.rest.map(f));
     }
 
     public <Y> Y accept(ListVisitor<X,Y> v) {
-	return v.visitCons(this.first, this.rest);
+        return v.visitCons(this.first, this.rest);
     }
 
 }
@@ -97,17 +97,17 @@ class Examples {
     List<Integer> is = new Cons<Integer>(5);
     List<List<Integer>> iss = new Cons<List<Integer>>(is);
     void testRev(Tester t) {
-	// the type safe way to write is.first + 3.
-	t.checkExpect(is.accept(new GetFirst()) + 3, 8);
-	// How to test exceptional behavior
-	t.checkException(new RuntimeException("You suck"),
-			 new Empty<Integer>(),
-			 "accept",
-			 new GetFirst());			 
-	t.checkExpect(is.rev(), is);
+        // the type safe way to write is.first + 3.
+        t.checkExpect(is.accept(new GetFirst()) + 3, 8);
+        // How to test exceptional behavior
+        t.checkException(new RuntimeException("You suck"),
+                         new Empty<Integer>(),
+                         "accept",
+                         new GetFirst());                        
+        t.checkExpect(is.rev(), is);
     }
 
     void testMap(Tester t) {
-	t.checkExpect(is.map(new IntToStr()), new Cons<String>("5"));
+        t.checkExpect(is.map(new IntToStr()), new Cons<String>("5"));
     }
 }
